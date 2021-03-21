6ccbea69ed82921f67e2cc6d24a9473e3efc722a
Georg Heiler <georg.kf.heiler@gmail.com>
2021-03-21 10:46:07 +0100

    fix setup.py

--- use_cases/use_case_b/setup.py
+++ use_cases/use_case_b/setup.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
     setup(
         name="use_case_b",
         author="geoHeil",
-        author_email="hello@elementl.com",
+        author_email="heilerg@csh.ac.at",
         license="Apache-2.0",
-        url="https://github.com/dagster-io/dagster/tree/master/examples/docs_snippets",
+        url="https://github.com/geoHeil/dagster-demo/tree/master/use_cases/use_case_b",
         classifiers=[
             "Programming Language :: Python :: 3.8",
             "License :: OSI Approved :: Apache Software License",
             "Operating System :: OS Independent",
         ],
-        packages=find_packages(exclude=["test"]),
+        #packages=find_packages(exclude=["test"]),
         install_requires=["dagster"],
         # extras_require={"full": ["seaborn", "scikit-learn"]},
     )
